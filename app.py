6f5d1c7d0263ceb97df4564b6d2b0ce2eda297a8
Shivamsingh92 <125203900+Shivamsingh92@users.noreply.github.com>
2026-03-06 20:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import pandas as pd
 import os
+import zipfile
 
 st.set_page_config(page_title="NTPC CSR Admit Card", page_icon="📄")
 
 st.title("NTPC CSR Entrance Examination 2026")
 st.subheader("Download Your Admit Card")
+
+# -------- unzip folder if needed --------
+zip_path = "New Folder.zip"
+extract_path = "id_cards"
+
+if os.path.exists(zip_path) and not os.path.exists(extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
 
 # Load Excel
 df = pd.read_excel("admission data spreadsheet.xlsx")
@@ -33,7 +42,7 @@ if st.button("Get Admit Card"):
                 name = student.iloc[0]["Candidate Name"]
                 name = name.strip().replace(" ", "_")
 
-                filename = f"New Folder/{roll}_{name}_AdmitCard.pdf"
+                filename = f"id_cards/{roll}_{name}_AdmitCard.pdf"
 
                 if os.path.exists(filename):
 
@@ -48,7 +57,7 @@ if st.button("Get Admit Card"):
                         )
 
                 else:
-                    st.error("Admit Card file not found in folder")
+                    st.error("Admit Card file not found")
 
             else:
                 st.error("Invalid Roll Number or Mobile Number")
